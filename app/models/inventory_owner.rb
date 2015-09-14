0242c61dd46619238c6ae6bc6511f07c0a3f1847
dylanpavelko <dylan@dylanpavelko.com>
2015-09-13 23:29:32 -0700

    allow better support of multi-ownership tracking

--- app/models/inventory_owner.rb
+++ app/models/inventory_owner.rb
@@ -1,6 +1,6 @@
 class InventoryOwner < ActiveRecord::Base
   belongs_to :inventory_item, class_name: "InventoryItem", foreign_key: "inventory_item_id"
-  belongs_to :quantity_type
+  belongs_to :quantity_type, class_name: "QuantityType", foreign_key: "quantity_type_id"
   belongs_to :person
   belongs_to :stored_in, class_name: "InventoryItem", foreign_key: "stored_in_id"
 
