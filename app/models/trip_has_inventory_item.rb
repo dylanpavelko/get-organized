ecdb1852004db574947952efc941de4c4a09c62e
Dylan Pavelko <dpavelko@gmail.com>
2015-09-26 21:37:57 +0000

    Better styling for pack lists side by side

--- app/models/trip_has_inventory_item.rb
+++ app/models/trip_has_inventory_item.rb
@@ -2,6 +2,8 @@ class TripHasInventoryItem < ActiveRecord::Base
   belongs_to :trip
   belongs_to :inventory_item
   
+  validates :trip_id, :presence => true
+  
   def get_amount
     if self.amount == nil
       return 1
