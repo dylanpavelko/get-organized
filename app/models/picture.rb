820ea7133d3790f01d3efc4d99103a21b7028395
dylanpavelko <dylan@dylanpavelko.com>
2016-04-10 21:35:35 -0700

    trying to get s3 working

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,6 +4,14 @@ class Picture < ActiveRecord::Base
 					:url => "/assets/pictures/:id/:basename.:extension",
 					:path => ":rails_root/public/assets/pictures/:id/:basename.:extension"
 
+	styles: {
+    thumb: ["40x40#", :png],
+    small: ["400x400>", :png]},
+    default_url: "#{Rails.root}/app/assets/images/missing.jpg",
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml",
+    path: ":rails_root/public/assets/pictures/:id/:basename.:extension"
+    				
 	validates_attachment :image, :size => { :in => 0..1000.kilobytes }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
