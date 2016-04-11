4a41a406a5119554236d7c0c05773808acd51289
dylanpavelko <dylan@dylanpavelko.com>
2016-04-10 22:00:59 -0700

    kind of works on local

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,13 +2,14 @@ class Picture < ActiveRecord::Base
 
 	has_attached_file :image,
 					:url => "/assets/pictures/:id/:basename.:extension",
-					:path => ":rails_root/public/assets/pictures/:id/:basename.:extension"
-					styles: {
- 				    thumb: ["40x40#", :png],
-				    small: ["400x400>", :png]},
-				    default_url: "#{Rails.root}/app/assets/images/missing.jpg",
-				    storage: :s3,
-				    s3_credentials: "#{Rails.root}/config/s3.yml",
+					:path => ":rails_root/public/assets/pictures/:id/:basename.:extension",
+					#:styles {
+ 				    #:thumb ["40x40#", :png],
+				    #:small ["400x400>", :png]},
+				    #default_url: "#{Rails.root}/app/assets/images/missing.jpg",
+				    :storage => :s3,
+				    :bucket => 'get-organized-assets',
+				    :s3_credentials => "#{Rails.root}/config/application.yml"
 #    path: ":rails_root/public/assets/pictures/:id/:basename.:extension"
 
     				
