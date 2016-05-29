e7ab7fc871bb69a442477fc00f1942158587ccc4
dylanpavelko <dylan@dylanpavelko.com>
2016-05-29 16:15:38 -0700

    was adjustments

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,12 +8,15 @@ class Picture < ActiveRecord::Base
 				    #:small ["400x400>", :png]},
 				    #default_url: "#{Rails.root}/app/assets/images/missing.jpg",
 				    :storage => :s3,
-				    :bucket => 'get-organized-assets',
-				    :s3_credentials => "#{Rails.root}/config/application.yml",
+				    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
 				    :url => ":s3_domain_url"
 #    path: ":rails_root/public/assets/pictures/:id/:basename.:extension"
 
-    				
+    		
+  def s3_credentials
+    {:bucket => 'get-organized-assets', :access_key_id => 'AKIAIJG6LK3XW5RZ22VA', :secret_access_key => Rails.application.secrets.aws_secret_access_key}
+  end
+
 	validates_attachment :image, :size => { :in => 0..1000.kilobytes }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
