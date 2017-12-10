0cb74ffcff8760ba37f7c281e05f7f5f1c24bbb5
Dylan Pavelko <dpavelko@gmail.com>
2017-12-10 00:21:24 +0000

    fix stock quotes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,4 +4,12 @@ class Stock < ActiveRecord::Base
   def quote
   	stocks = StockQuote::Stock.quote(self.symbol)
   end
+  
+  def get_last_price
+    @response = Net::HTTP.get_response("finance.google.com","/finance?q=" + self.symbol + "&output=json")
+    @response.body.slice!(0, 4)
+    @response = ActiveSupport::JSON.decode(@response.body)
+    return @response[0]['l'].to_f
+  end
+  
 end
