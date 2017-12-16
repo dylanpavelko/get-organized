ce1a021d316f726b1af3107853e510d964a6b1ef
Dylan Pavelko <dpavelko@gmail.com>
2017-12-16 01:54:40 +0000

    clean up

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -5,18 +5,14 @@ class FoodItem < ActiveRecord::Base
     end
     
     def get_average_price_for_amount(amount, unit_type)
-        @options = get_Inventory_Options
-        if @options != nil 
-            if @options.count > 0
-                @price = @options.first.inventory_item.average_price_by(unit_type)
-                if amount != ""
-                    @price = @price * amount.to_f
-                end
-                return @price
-            else
-                return nil
+        @options = self.get_Inventory_Options
+        if @options.count > 0
+            @price = @options.first.inventory_item.average_price_by(unit_type)
+            if amount != ""
+                @price = @price * amount.to_f
             end
-        else 
+            return @price
+        else
             return nil
         end
     end
