875117cafeea28a124e3e7adb757cdfe1025abc8
Dylan Pavelko <dpavelko@gmail.com>
2019-10-04 05:00:31 +0000

    has file

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
 
-	has_attached_file :image,
+	has_one_attached :image,
 					#:url => "/assets/pictures/:id/:basename.:extension",
 					:path => ":rails_root/public/assets/pictures/:id/:basename.:extension",
 					#:styles {
