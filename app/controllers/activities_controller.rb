80abea921520ef0d58d180cac830d1e021e94bcf
Dylan Pavelko <dpavelko@gmail.com>
2019-10-04 04:41:10 +0000

    before action

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
-  before_filter :authenticate_user
-  before_filter :authorized_only
+  before_action :authenticate_user
+  before_action :authorized_only
 
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
