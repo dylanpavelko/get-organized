70992d84514c4bfaa11133443967261a830dd2f4
Dylan Pavelko <dpavelko@gmail.com>
2020-01-02 04:41:40 +0000

    adding pictures back hopefully

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,13 +1,13 @@
 class Picture < ActiveRecord::Base
 
-	has_one_attached :image
+	has_attached_file :image
 
     		
   def s3_credentials
     {:bucket => 'get-organized-assets', :access_key_id => 'AKIAIJG6LK3XW5RZ22VA', :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
   end
 
-	#validates_attachment :image, :size => { :in => 0..1000.kilobytes }
-	#validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	validates_attachment :image, :size => { :in => 0..1000.kilobytes }
+	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 end
